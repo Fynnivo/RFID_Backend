cd639dbbf17e8bf25acd2fdb2e142efc1597af9b
Fynnivo <101862182+Fynnivo@users.noreply.github.com>
2026-01-10 06:58:07 +0700

    Update CORS origin to production URL

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ const app = express();
 // Middleware
 app.use(helmet());
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['https://rfid-project-3keb.vercel.app'],
   credentials: true
 }));
 app.use(compression());
